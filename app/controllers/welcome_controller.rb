2b5985d0e930b1dd13f88d5ab3a4486a563e9f79
Joshua de Lange <joshua.delange@gmail.com>
2013-08-20 00:03:46 +0200

    giving the front page some of our newfound data

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+
     @websites = Website.all()
+
+    @top_words = Word.find(
+      :all,
+      :select => "word, count(*) as count",
+      :group => "word",
+      :order => "count desc",
+      :limit => 20
+    )
+
   end
 end
