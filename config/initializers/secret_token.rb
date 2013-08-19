40f39a9347a7ce9a7f768e3982f11629582e2c1d
Joshua de Lange <joshua.delange@gmail.com>
2013-08-19 17:24:22 +0200

    first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YwamIpsum::Application.config.secret_token = 'c040875a031e593cb37525a3aad0a080d28155c7cb80ad526f3a4094c8f5d2b14e84c1f00e82103beb835827afd5fc51d84efb4e323b0820c39df1416f7abaec'
